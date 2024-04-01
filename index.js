ffa574ff72c6bd672551797db17d4349305bc38b
jevansHC <50111172+jevansHC@users.noreply.github.com>
2024-04-01 17:21:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
     console.log(interaction.data.name)
 
     if(interaction.data.name == 'igp'){
-      console.log('Message received! Message content: ' + interaction.data.options[0]);
+      console.log('Message received! Message content: ' + interaction.data.options.name);
  
       let circuit = interaction.data.options[0].value;
       if (interaction.data.options.name=='info') {
